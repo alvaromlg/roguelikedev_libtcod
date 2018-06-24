803ece9c770ae4675a564a2cc5bfd49cc4dcb9ea
Alvaro <apelegrinafernandez@gmail.com>
2018-06-24 15:22:03 +0200

    moving around

--- engine.py
+++ engine.py
@@ -1,24 +1,46 @@
 import libtcodpy as libtcod
 
+from input_handlers import handle_keys
+
 def main():
     screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     # define font to use
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     # configure and create the screen
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
     # game loop
     while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
 
-        key = libtcod.console_check_for_keypress()
+        action = handle_keys(key)
 
-        if key.vk == libtcod.KEY_ESCAPE:
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
             return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
 
 if __name__ == '__main__':
     main()
